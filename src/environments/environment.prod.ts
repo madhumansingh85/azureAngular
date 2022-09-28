3ddb5216d1322d1bc0014b389feedff466230b46
guruprasanthe <guru.prasanth.e@gds.ey.com>
2022-09-28 17:40:26 +0530

    changes in env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   // apiURL: 'http://20.12.218.228/node1/',
-  apiURL: 'https://nodeapp1fordmp.azurewebsites.net/',
-  socketURL: 'https://nodeapp2fordmp.azurewebsites.net/',
+  apiURL: 'https://azurenodedemo2.azurewebsites.net/'
 };
